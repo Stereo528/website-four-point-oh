e281f997d44d263897556e094ff8835400d307b5
Stereo528 <studio528.github@gmail.com>
2023-05-19 23:28:43 -0600

    setup tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
+<h1 class="text-4xl underline">Welcome to SvelteKit</h1>
+<style lang="postcss">
+  :global(html) {
+    font-family: sans-serif;
+    text-align: center;
+    background-color: theme(colors.gray.300);
+  }
+</style>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
